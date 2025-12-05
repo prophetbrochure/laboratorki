f45a300c1c56a1e51cd71af8ea9f22c2718d6ab8
prophetbrochure <prophetbrochure@gmail.com>
2025-12-05 15:08:36 +0600

    [Laba4_1] PriorityQueueImplementation2

--- src/Laba4_MinSearch/Laba4_2/Main.java
+++ src/Laba4_MinSearch/Laba4_2/Main.java
@@ -3,18 +3,20 @@ package Laba4_MinSearch.Laba4_2;
 public class Main {
     public static void main(String[] args) {
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueueImplementation1();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueueImplementation2();
 
-        for (int i = 0; i < Math.pow(2, 4) - 1; i++) {
-            priorityQueue.enqueue(2);
+
+
+      for (int i = (int) Math.pow(2, 4) - 1 ; i > 0; i--) {
+           priorityQueue.enqueue(i);
+      }
+
+       priorityQueue.increment(1, 10);
+
+        for(int i = 0; i < Math.pow(2, 5)-1; i++){
+            priorityQueue.dequeueMax();
         }
 
-        priorityQueue.increment(1, 10);
-
-//        for(int i = 0; i < Math.pow(2, 4)-1; i++){
-//            priorityQueue.dequeueMax();
-//        }
-        priorityQueue.dequeueMax();
 
 
     }

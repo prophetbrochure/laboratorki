c35f2c983bc93c6a20c39dfb7caa8537be4517e5
prophetbrochure <prophetbrochure@gmail.com>
2025-12-05 13:51:50 +0600

    [Laba4_1] PriorityQueueImplementation1 final commit

--- src/Laba4_MinSearch/Laba4_2/Main.java
+++ src/Laba4_MinSearch/Laba4_2/Main.java
@@ -1,15 +1,20 @@
 package Laba4_MinSearch.Laba4_2;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueueImplementation1<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueueImplementation1();
 
-        for(int i = 0; i < Math.pow(2, 19)-1; i++){
+        for (int i = 0; i < Math.pow(2, 4) - 1; i++) {
             priorityQueue.enqueue(2);
         }
 
+        priorityQueue.increment(1, 10);
 
+//        for(int i = 0; i < Math.pow(2, 4)-1; i++){
+//            priorityQueue.dequeueMax();
+//        }
+        priorityQueue.dequeueMax();
 
 
     }
--- src/Laba4_MinSearch/Laba4_2/PriorityQueueImplementation1.java
+++ src/Laba4_MinSearch/Laba4_2/PriorityQueueImplementation1.java
@@ -1,14 +1,14 @@
 package Laba4_MinSearch.Laba4_2;
 
-public class PriorityQueueImplementation1<T extends Comparable<T>> implements PriorityQueue<T> {
+public class PriorityQueueImplementation1 implements PriorityQueue<Integer> {
     private class Node {
         long i;
-        T data;
+        Integer data;
         final Node parent;
         Node left = null;
         Node right = null;
 
-        Node(long i, T data, Node parent) {
+        Node(long i, Integer data, Node parent) {
             this.i = i;
             this.parent = parent;
             this.data = data;
@@ -16,7 +16,7 @@ public class PriorityQueueImplementation1<T extends Comparable<T>> implements Pr
     }
 
     void swapValues(Node a, Node b) {
-        T tempData = a.data;
+        Integer tempData = a.data;
         a.data = b.data;
         b.data = tempData;
         long tempI = a.i;
@@ -28,27 +28,31 @@ public class PriorityQueueImplementation1<T extends Comparable<T>> implements Pr
         return (a.data.compareTo(b.data) > 0) ? a : b;
     }
 
-//    Node recursiveSearch(Node node, long searchedI){
-//        if(node.i == searchedI){
-//            return node;
-//        }
-//
-//    }
+    private Node recursiveSearch(Node node, long i) {
+        if (node == null) return null;
+        if (node.i == i) return node;
+
+        Node left = recursiveSearch(node.left, i);
+        if (left != null) return left;
+
+        return recursiveSearch(node.right, i);
+    }
+
 
     Node head;
-    long i;
+    long additionId;
     long pointer = 1;
     boolean isEmpty = true;
 
 
     @Override
-    public void enqueue(T newData) {
+    public void enqueue(Integer newData) {
         if (isEmpty) {
-            System.out.println("Добавляем корневой узел " + pointer);
-            head = new Node(i, newData, null);
+            System.out.println("Добавляем корневой узел " + pointer + "\n");
+            head = new Node(additionId, newData, null);
             isEmpty = false;
         } else {
-            System.out.println("\nДобавляем узел " + pointer);
+            System.out.println("Добавляем узел " + pointer);
             int height = (int) (Math.log(pointer) / Math.log(2));   // Math.log(x) = ln(x)
             long newPointer = pointer - (long) Math.pow(2, height);
             Node currentNode = head;
@@ -64,13 +68,13 @@ public class PriorityQueueImplementation1<T extends Comparable<T>> implements Pr
                 }
             }
             if (currentNode.left == null) {
-                System.out.println("Записали узел " + pointer + " в левую ветвь");
-                currentNode.left = new Node(i, newData, currentNode);
+                System.out.println("Записали узел " + pointer + " в левую ветвь\n");
+                currentNode.left = new Node(additionId, newData, currentNode);
                 currentNode = currentNode.left;
 
             } else {
-                System.out.println("Записали узел " + pointer + " в правую ветвь");
-                currentNode.right = new Node(i, newData, currentNode);
+                System.out.println("Записали узел " + pointer + " в правую ветвь\n");
+                currentNode.right = new Node(additionId, newData, currentNode);
                 currentNode = currentNode.right;
             }
             while (currentNode.parent != null) {  // просеивание снизу вверх
@@ -83,28 +87,94 @@ public class PriorityQueueImplementation1<T extends Comparable<T>> implements Pr
             }
         }
         pointer++;
-        i++;
+        additionId++;
     }
 
     @Override
     public void dequeueMax() {
         if (isEmpty) {
-            System.out.println("*");
+            System.out.println("Попытка удалить из пустой очереди\n");
             return;
         } else {
-            // TODO
+            System.out.println("Удаляем узел " + (pointer - 1) + "\n");
+            int height = (int) (Math.log(pointer - 1) / Math.log(2));   // Math.log(x) = ln(x)
+            long newPointer = pointer - 1 - (long) Math.pow(2, height);
+            Node currentNode = head;
+
+            for (int i = height - 1; i > 0; i--) {
+                if (newPointer >= (long) Math.pow(2, i)) {
+                    currentNode = currentNode.right;
+                    newPointer -= (long) Math.pow(2, i);
+                } else {
+                    currentNode = currentNode.left;
+                }
+            }
+            if (currentNode.right != null) {
+                swapValues(head, currentNode.right);
+                currentNode.right = null;
+            } else if (currentNode.left != null){
+                swapValues(head, currentNode.left);
+                currentNode.left = null;
+            }
+            currentNode = head; // просеивание сверху вниз
+            while (!(currentNode.left == null && currentNode.right == null)) {
+                if (currentNode.right == null) {
+                    if (currentNode.data.compareTo(currentNode.left.data) < 0) {
+                        currentNode = currentNode.left;
+                        swapValues(currentNode, currentNode.parent);
+                    } else {
+                        break;
+                    }
+                }
+                if (currentNode.data.compareTo(currentNode.left.data) < 0 || currentNode.data.compareTo(currentNode.right.data) < 0) {
+                    currentNode = currentNode.left.data.compareTo(currentNode.right.data) > 0 ? currentNode.left : currentNode.right;
+                    swapValues(currentNode, currentNode.parent);
+                } else {
+                    break;
+                }
+            }
         }
         pointer--;
+        if(pointer == 1){
+            isEmpty = true;
+        }
     }
 
     @Override
-    public void increment(long operation, T addition) {
+    public void increment(long id, java.lang.Integer addition) {   // Зачем вообще было использовать дженерики, если в этой сигнатуре дан Integer???
         if (isEmpty) {
-            System.out.println("Queue is empty!");
+            System.out.println("Попытка увеличить элеммент в пустой очереди!\n");
         }
-//        else{
-//              // найти элемент, увеличить data на 5 и просеять снизу вверх
-//
-//        }
+
+        Node result = recursiveSearch(head, id);
+
+        if(result == null){
+            System.out.println("Попытка увеличить несуществующий узел");
+            return;
+        }
+
+        System.out.println("Увеличиваем элемент с id " + result.i + " на" + addition);
+
+        result.data += addition;
+
+        Node currentNode = result; // просеивание сверху вниз
+        while (!(currentNode.left == null && currentNode.right == null)) {
+            if (currentNode.right == null) {
+                if (currentNode.data.compareTo(currentNode.left.data) < 0) {
+                    currentNode = currentNode.left;
+                    swapValues(currentNode, currentNode.parent);
+                } else {
+                    break;
+                }
+            }
+            if (currentNode.data.compareTo(currentNode.left.data) < 0 || currentNode.data.compareTo(currentNode.right.data) < 0) {
+                currentNode = currentNode.left.data.compareTo(currentNode.right.data) > 0 ? currentNode.left : currentNode.right;
+                swapValues(currentNode, currentNode.parent);
+            } else {
+                break;
+            }
+        }
+
+
     }
 }
